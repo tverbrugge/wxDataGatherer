260df9c2b30996b04081f1136e306e63f1f8ba58
troy.verbrugge <tverbrugge@gmail.com>
2019-02-12 19:25:06 -0700

    Checking in before calling it a night?

--- src/main/java/tvsoft/net/NCDCConnection.java
+++ src/main/java/tvsoft/net/NCDCConnection.java
@@ -1,20 +1,62 @@
 package tvsoft.net;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Properties;
 
+import javax.net.ssl.HttpsURLConnection;
+
 public class NCDCConnection {
+	
+	// See https://stackoverflow.com/questions/24454164/what-will-be-the-equivalent-to-following-curl-command-in-java
 	public static void main(String[] args) {
-		Properties props = new Properties();
-		props.setProperty("token", "NOPE");
-		try {
-			props.storeToXML(Files.newOutputStream(Paths.get("./config/ncdcSecurity.xml")), "some comment");
+
+		String data = "key={authKey}";
+		byte[] bytes = data.getBytes(StandardCharsets.UTF_8);
+		int length = bytes.length;
+		try (OutputStream os = getConnection().getOutputStream()) {
+			os.write(bytes, 0, length);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	private static HttpsURLConnection getConnection() {
+		HttpsURLConnection connection;
+		try {
+			connection = (HttpsURLConnection) new URL("http://example.com/").openConnection();
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		try {
+			connection.setRequestMethod("GET");
+		} catch (ProtocolException e) {
+			throw new RuntimeException(e);
+		}
+//		connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+//		connection.setRequestProperty("charset", "UTF-8");
+//		connection.setRequestProperty("Content-Length", String.valueOf(length));
+		connection.setDoInput(true);
+		connection.setDoOutput(true);
+
+		return connection;
+	}
+
+	private static Properties getConnectionProperties() {
+		Properties props = new Properties();
+		try {
+			props.loadFromXML(Files.newInputStream(Paths.get("./config/ncdcSecurity.xml")));
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		return props;
 	}
 
 }
